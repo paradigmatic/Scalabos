cf8388ad0e7227f8e4a0c9351a2338262e3fc06a
JL Falcone <jean-luc.falcone@unige.ch>
2010-01-23 13:57:45 +0100

    Multiselection support

--- src/Selection.scala
+++ src/Selection.scala
@@ -4,6 +4,23 @@ import lb._
 
 trait Region {
    def foreach[D <: Descriptor]( lattice: Lattice2D[D], f: (Cell[D]) => Unit ): Unit
+  def and( other: Region ):Region = MultiRegion( this, other )
+  def &( other: Region )  = and( other )
+}
+
+case class MultiRegion( r1: Region, r2: Region ) extends Region {
+
+  private var regions = List( r1, r2  )
+  
+  def foreach[D <: Descriptor]( lattice: Lattice2D[D], f: (Cell[D]) => Unit ) {
+    regions.foreach( _.foreach( lattice, f ) )
+  }
+  
+  override def and( other: Region ) = {
+    regions ::= other
+    this
+  }
+
 }
 
 case class Rectangle( val fromX: Int, val toX: Int,
