cc20f0d68bd4d3494fc8d3670b9598f364228e5e
JL Falcone <jean-luc.falcone@unige.ch>
2010-01-24 21:06:23 +0100

    Added colobar

--- src/visual/Visual.scala
+++ src/visual/Visual.scala
@@ -9,10 +9,15 @@ import org.jfree.chart.renderer.GrayPaintScale
 import org.jfree.chart.JFreeChart
 import org.jfree.chart.ChartPanel
 import java.awt.Dimension
+import java.awt.Color
+import java.awt.Font
 import java.io.File
 import org.jfree.ui.ApplicationFrame
 import org.jfree.chart.ChartUtilities
-
+import org.jfree.chart.title.PaintScaleLegend
+import org.jfree.chart.axis.AxisLocation
+import org.jfree.ui.RectangleInsets
+import org.jfree.ui.RectangleEdge
 
 
 object MatrixDataset {
@@ -58,17 +63,43 @@ class Image( private val matrix: Array[Array[Double]] ) {
     }
   }
 
-  lazy val chart = {
+  lazy val paintScale =  new GrayPaintScale( minMax._1, minMax._2 )
+
+  lazy val plot = {
     val xAxis = new NumberAxis("x Axis")
     val yAxis = new NumberAxis("y Axis")
     val renderer = new XYBlockRenderer
-    val paintScale = new GrayPaintScale( minMax._1, minMax._2 )
     renderer setPaintScale paintScale
     renderer setBlockHeight 1.0
     renderer setBlockWidth 1.0
-    val plot = new XYPlot(dataset, xAxis, yAxis, renderer )
+    new XYPlot(dataset, xAxis, yAxis, renderer )
+  }
+
+  lazy val legend = {
+    val scaleAxis = new NumberAxis("Scale")
+    scaleAxis setUpperBound minMax._2
+    scaleAxis.setAxisLinePaint(Color.white)
+    scaleAxis.setTickMarkPaint(Color.white)
+    scaleAxis.setTickLabelFont(new Font("Dialog", Font.PLAIN, 12))
+    val  legend = new PaintScaleLegend(paintScale, scaleAxis)
+    legend.setSubdivisionCount(100)
+    legend.setAxisLocation(AxisLocation.TOP_OR_RIGHT)
+    //legend.setStripOutlineVisible(true)
+    legend.setStripOutlinePaint(Color.BLACK)
+    //scaleAxis.setTickLabelFont(new Font("Arial",12,0))
+    //legend.setAxisOffset(5.0);
+    //legend.setMargin(new RectangleInsets(5, 20, 5, 5))
+    legend.setPadding(new RectangleInsets(5, 20, 5, 5))
+    legend.setStripWidth(20)
+    legend.setPosition(RectangleEdge.RIGHT)
+    legend.setBackgroundPaint(Color.WHITE)
+    legend
+  }
+ 
+  lazy val chart = {
     val chart = new JFreeChart("Chart Title",JFreeChart.DEFAULT_TITLE_FONT,plot,true)
     chart.removeLegend()
+    chart.addSubtitle(legend);
     chart
   }
 
@@ -130,4 +161,10 @@ class Image( private val matrix: Array[Array[Double]] ) {
         return chart;
     }
 */
+}
+
+object Image {
+
+  def apply(matrix: Array[Array[Double]] ) = new Image(matrix)
+
 }
