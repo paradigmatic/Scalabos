9dce8397bc5a0ec445825a38f469eabef34792a8
Orestis Malaspinas <malaspin@pc7119.(none)>
2010-01-28 14:17:08 +0100

    Added average computation of the density in your face. Bastard.

--- src/dataProcessing/dataProcessors2D.scala
+++ src/dataProcessing/dataProcessors2D.scala
@@ -5,6 +5,17 @@ import lb.util._
 
 object Averages {
   
+  def density[T <: Descriptor](lattice:Lattice2D[T], domain:Region) : Double  = {
+    //     var totEnergy = 0.5 * lattice.map( C => Arrays.normSqr(C.u)).flatMap( x => x ).reduceLeft(_+_)
+    val latticeDomain = lattice.select(domain)
+    val totCells = latticeDomain.indices.size
+    
+    var totEnergy = 0.5 * latticeDomain.map( C => C.rho).reduceLeft(_+_)
+    totEnergy /= totCells
+    totEnergy
+  }
+  
+  
   def energy[T <: Descriptor](lattice:Lattice2D[T], domain:Region) : Double  = {
 //     var totEnergy = 0.5 * lattice.map( C => Arrays.normSqr(C.u)).flatMap( x => x ).reduceLeft(_+_)
     val latticeDomain = lattice.select(domain)
