101895fade3ee6ac16de42a51482300787f7c572
JL Falcone <jean-luc.falcone@unige.ch>
2010-01-29 19:55:52 +0100

    Added vector operations

--- src/math/Vec.scala
+++ src/math/Vec.scala
@@ -2,9 +2,13 @@ package lb.math
 
 abstract class Vec[V,T]( val size: Int ) {
   self: V =>
-  def +( other: V ): V
   def apply( i: Int ): T
-  def *( other: V): T
+  def +( other: V ): V
+  def -( other: V ): V
+  def unary_-(): V
+  def **( other: V): T
+  def *( scalar: T ): V
+  def /( scalar: T ): V
   def norm2(): Double
   lazy val norm = Math.sqrt( norm2 )
   def toArray(): Array[T]
@@ -22,11 +26,16 @@ abstract class Vec2[T]( val x: T, val y: T ) extends Vec[Vec2[T],T](2) {
     ary(1) = y
     ary
   }
+  override lazy val toString = "Vec(" + x + ", " + y + ")"
 }
 
 class Vec2Double( x: Double, y: Double ) extends Vec2[Double](x,y){
+  def unary_-() = new Vec2Double( -x, -y )
   def +( other: Vec2[Double] ) = new Vec2Double( x + other.x, y + other.y )
-  def *( other: Vec2[Double] ) = x*other.x + y*other.y
+  def -( other: Vec2[Double] ) = new Vec2Double( x - other.x, y - other.y )
+  def **( other: Vec2[Double] ) = x*other.x + y*other.y
+  def *( scalar: Double ) = new Vec2Double( scalar*x, scalar*y )
+  def /( scalar: Double ) = new Vec2Double( scalar/x, scalar/y )
   lazy val norm2 = x*x + y*y
 
 } 
